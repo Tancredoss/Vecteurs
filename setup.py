193f005bbde2c5743873c9e467ac38fc0b6ef7f7
Tancredoss <107503215+Tancredoss@users.noreply.github.com>
2023-08-15 12:31:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
   
 setup(
     name='vectors',
-    version='1.0',
+    version='1.1',
     description='A implementation of vectors in python',
     author='Tancrède Tonini',
 )
