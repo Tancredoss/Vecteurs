a7628ffc91adfcbc5489df638be5d77ea4007b3f
Tancredoss <107503215+Tancredoss@users.noreply.github.com>
2023-08-15 12:08:46 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+  
+setup(
+    name='vecteur',
+    version='1.0',
+    description='A implementation of vectors in python',
+    author='Tancrède Tonini',
+    packages=['vecteur'],
+)
